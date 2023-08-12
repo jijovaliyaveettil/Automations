f171d48a50827e553b95f6855b54deb0c55b57e0
jijovaliyaveettil <v.francisjijo@gmail.com>
2023-08-12 14:54:14 +0200

    Added User Registration page

--- Basic Progs/test.py
+++ Basic Progs/test.py
@@ -31,16 +31,27 @@
 # print(tuple)
 # print(c)
 
-arr = [2, 3, 2, 3, 5]
-N=5
-hashmap = {}
-for i in range(N):
-    if(arr[i] in hashmap ):
-        count = count + 1
-        hashmap[i] = count
-    else:
-        count = 1
-        hashmap[i] = count
+# arr = [2, 3, 2, 3, 5]
+# N=5
+# hashmap = {}
+# for i in range(N):
+#     if(arr[i] in hashmap ):
+#         count = count + 1
+#         hashmap[i] = count
+#     else:
+#         count = 1
+#         hashmap[i] = count
         
-for i in range(N):
-    print(hashmap[arr[i]])
+# for i in range(N):
+#     print(hashmap[arr[i]])
+
+# s = "anagram"
+# t = "nagaram"
+
+# s_sorted = sorted(s)
+# t_sorted = sorted(s)
+
+# if s_sorted == t_sorted:
+#     print(s[0]) 
+# else:
+#     print(False)
--- Django/mysite/main/urls.py
+++ Django/mysite/main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("<int:id>", views.index, name = "index"),
     path("", views.home, name = "home"),
+    path("home/", views.home, name = "home"),
+    path("<int:id>", views.index, name = "index"),
     path("create/", views.create, name = "create"),
 ]
