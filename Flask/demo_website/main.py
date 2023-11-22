3c3aeb3b0778d81c450635fde2b8f2f01c5d5acc
jijovaliyaveettil <37544499+jijovaliyaveettil@users.noreply.github.com>
2023-11-22 19:00:14 +0100

    Delete Flask directory

--- Flask/demo_website/main.py
+++
@@ -1,7 +0,0 @@
-from website import create_app
-
-# intialize app
-app = create_app()
-
-if __name__ == '__app__': #run the webserver only through this file
-    app.run(debug =True)  #re-run automatically
--- Flask/demo_website/templates/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'this_is_mykey'
-    
-    return app
